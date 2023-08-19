3b110c9d382060907432c84c2e82aa63500a2a1d
smin0820 <97029953+smin0820@users.noreply.github.com>
2023-08-20 07:39:14 +0900

    style: Header 스타일링

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <header>
-            <Link>
+        <header className='flex justify-between bg-symbol px-4 py-3 text-white font-bold'>
+            <Link className='text-2xl'>
                 <h1>카타르 아시안컵</h1>
             </Link>
-            <nav>
+            <nav className='text-base flex items-center gap-4'>
                 <Link to='/roster'>선수 명단</Link>
                 <Link to='/cheer'>응원하기</Link>
                 <Link to='/manager'>내가 감독</Link>
